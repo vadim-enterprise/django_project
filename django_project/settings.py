1cf07c95b667f6d4fc5b29bc7ebfd647a4f56735
Ubuntu <ubuntu@ip-172-31-44-162.ec2.internal>
2024-11-17 09:44:11 +0000

    updated app

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-q5(d0j*erh^x=rjleyuz3a%5rbj5f#cn$l1$!cb5@5lygoq3vp"
 
-OPENAI_API_KEY = "sk-LqFYFnV9MF0xy9DmjPwMSLotYKdTj7gkKEIKkAs4lDT3BlbkFJYt01anCJS46iABMMq_OlT8mMtfLMoVGRK5YYgWu6AA"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
